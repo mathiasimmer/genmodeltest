0cd68a51212dd75d79b135a9576dd78148b0bfc2
maimm10@student.sdu.dk <help@genmymodel.com>
2017-04-23 12:30:29 +0000

    commit by maimm10@student.sdu.dk using genmymodel: v20170423_123027

--- model.py
+++ model.py
@@ -1,93 +1,24 @@
 from abc import abstractmethod
-
-
-class Deros(object):
-    pass
-
-    # Start of user code -> properties/constructors for Deros class
-
-    # End of user code
-
-
-    def start(self, component):
-        # Start of user code protected zone for start function body
-        raise NotImplementedError
-        # End of user code	
-
-    def stop(self, component):
-        # Start of user code protected zone for stop function body
-        raise NotImplementedError
-        # End of user code	
-        # Start of user code -> methods for Deros class
-
-        # End of user code
-
-
-class IContainer(object):
-    pass
-
-    # Start of user code -> properties/constructors for IContainer class(interface)
-
-    # End of user code
-
-    @abstractmethod
-    def create(self):
-        # Start of user code protected zone for create function body
-        raise NotImplementedError
-        # End of user code	
-
-    @abstractmethod
-    def start(self):
-        # Start of user code protected zone for start function body
-        raise NotImplementedError
-        # End of user code	
-
-    @abstractmethod
-    def run(self):
-        # Start of user code protected zone for run function body
-        raise NotImplementedError
-        # End of user code	
-
-    @abstractmethod
-    def remove(self):
-        # Start of user code protected zone for remove function body
-        raise NotImplementedError
-        # End of user code	
-        # Start of user code -> methods for IContainer class(interface)
-
-        # End of user code
-
-
 class Service(object):
     def __initmodel__(self):
         self.name = ""
-
+        self.__orchestration = None
+        
         self.containers = []
-
+        
     # Start of user code -> properties/constructors for Service class
         self.orchestration = None
     # End of user code
-
-
+    
+    
     def __init__(self, orchestration):
         # Start of user code protected zone for __init__ function body
         self.__initmodel__()
 
         # End of user code	
-        # Start of user code -> methods for Service class
+    # Start of user code -> methods for Service class
 
         # End of user code
-
-
-class Component(object):
-    pass
-    # Start of user code -> properties/constructors for Component class
-
-    # End of user code
-    # Start of user code -> methods for Component class
-
-    # End of user code
-
 
 class IOrchestration(object):
     pass
@@ -98,6 +29,79 @@ class IOrchestration(object):
 
     # End of user code
 
+class Component(object):
+    pass
+    # Start of user code -> properties/constructors for Component class
+
+    # End of user code
+    # Start of user code -> methods for Component class
+
+    # End of user code
+
+class IContainer(object):
+    pass
+    # Start of user code -> properties/constructors for IContainer class(interface)
+
+    # End of user code
+    
+    @abstractmethod
+    def create(self):
+        # Start of user code protected zone for create function body
+        raise NotImplementedError
+        # End of user code	
+    
+    @abstractmethod
+    def start(self):
+        # Start of user code protected zone for start function body
+        raise NotImplementedError
+        # End of user code	
+    
+    @abstractmethod
+    def run(self):
+        # Start of user code protected zone for run function body
+        raise NotImplementedError
+        # End of user code	
+    
+    @abstractmethod
+    def remove(self):
+        # Start of user code protected zone for remove function body
+        raise NotImplementedError
+        # End of user code	
+    # Start of user code -> methods for IContainer class(interface)
+
+        # End of user code
+
+
+
+class Deros(object):
+    pass
+    # Start of user code -> properties/constructors for Deros class
+
+    # End of user code
+    
+    
+    def start(self, component):
+        # Start of user code protected zone for start function body
+        raise NotImplementedError
+        # End of user code	
+    
+    
+    def stop(self, component):
+        # Start of user code protected zone for stop function body
+        raise NotImplementedError
+        # End of user code	
+    # Start of user code -> methods for Deros class
+
+        # End of user code
+
+class DockerOrchestration(IOrchestration):
+    pass
+    # Start of user code -> properties/constructors for DockerOrchestration class
+
+    # End of user code
+    # Start of user code -> methods for DockerOrchestration class
+
+    # End of user code
 
 class DockerContainer(IContainer):
     pass
@@ -108,7 +112,6 @@ class DockerContainer(IContainer):
 
     # End of user code
 
-
 class Leaf(Component):
     pass
     # Start of user code -> properties/constructors for Leaf class
@@ -118,17 +121,18 @@ class Leaf(Component):
 
     # End of user code
 
-
 class Composite(Component):
     def __initmodel__(self):
+        
         self.components = []
-
-        # Start of user code -> properties/constructors for Composite class
-
-        # End of user code
-        # Start of user code -> methods for Composite class
+        
+    # Start of user code -> properties/constructors for Composite class
 
         # End of user code
+    # Start of user code -> methods for Composite class
+
+        # End of user code
+
 
 # Start of user code -> functions/methods for model package
 
