80a3048d817a9fa5fd278bcbcaff69498b9f99af
maimm10@student.sdu.dk <help@genmymodel.com>
2017-04-23 09:05:15 +0000

    commit by maimm10@student.sdu.dk using genmymodel: v20170423_090513

--- model.py
+++ model.py
@@ -1,10 +1,15 @@
+
 class IContainer(object):
     pass
     # Start of user code -> properties/constructors for IContainer class(interface)
 
     # End of user code
-    def lulz(self):
-        # Start of user code protected zone for lulz function body
+    def2 create(self):
+        # Start of user code protected zone for create function body
+        raise NotImplementedError
+        # End of user code	
+    def2 start(self):
+        # Start of user code protected zone for start function body
         raise NotImplementedError
         # End of user code	
     # Start of user code -> methods for IContainer class(interface)
@@ -12,7 +17,10 @@ class IContainer(object):
     # End of user code
 
 class Service(object):
-    pass
+    def __init__(self):
+        
+        self.containers = []
+        
     # Start of user code -> properties/constructors for Service class
 
     # End of user code
