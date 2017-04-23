b018cbb15f4bfbc490a2d2b4a86aca996a93d894
maimm10@student.sdu.dk <help@genmymodel.com>
2017-04-23 08:53:24 +0000

    commit by maimm10@student.sdu.dk using genmymodel: v20170423_085322

---
+++ model.py
@@ -0,0 +1,31 @@
+class IContainer(object):
+    pass
+    # Start of user code -> properties/constructors for IContainer class(interface)
+
+    # End of user code
+    # Start of user code -> methods for IContainer class(interface)
+
+    # End of user code
+
+class Service(object):
+    pass
+    # Start of user code -> properties/constructors for Service class
+
+    # End of user code
+    # Start of user code -> methods for Service class
+
+    # End of user code
+
+class DockerContainer(IContainer):
+    pass
+    # Start of user code -> properties/constructors for DockerContainer class
+
+    # End of user code
+    # Start of user code -> methods for DockerContainer class
+
+    # End of user code
+
+
+# Start of user code -> functions/methods for model package
+
+# End of user code
